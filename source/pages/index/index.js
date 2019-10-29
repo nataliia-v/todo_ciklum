906180aad68699462e0ebf032c464e446295a890
nataliia-v <nataly.verbenskaya@gmail.com>
2019-10-29 12:12:37 +0200

    fetching tasks

--- source/pages/index/index.js
+++ source/pages/index/index.js
@@ -31,7 +31,7 @@ document.getElementById('newTodoItem').addEventListener('submit', saveTodoItem);
 
 function saveTodoItem(e) {
 
-  e.preventDefault();
+  // e.preventDefault();
   // Get form values
   let titleTodo = document.getElementById('title').value;
   let descriptionTodo = document.getElementById('description').value;
@@ -43,6 +43,7 @@ function saveTodoItem(e) {
     description: descriptionTodo,
     priority: '',
     id: `f${ (+new Date).toString(16) }`,
+    status: true,
   };
 
   optionsArr.forEach((el) => {
@@ -63,7 +64,173 @@ function saveTodoItem(e) {
     console.log(localStorage);
   }
 
+  // Clear form
+  document.getElementById('newTodoItem').reset();
+
+  // Re-fetch todos
+  fetchTodos();
+
+  // Prevent form from submitting
+  e.preventDefault();
+
 }
+
+function deleteCurrentTodoItem(id) {
+
+  // Get todoItems from localStorage
+  let todoItems = JSON.parse(localStorage.getItem('todos'));
+  // Loop through the todoItems
+  for(let i =0; i < todoItems.length; i++){
+    if(todoItems[i].id === id){
+      // Remove from array
+      todoItems.splice(i, 1);
+    }
+  }
+  // Re-set back to localStorage
+  localStorage.setItem('bookmarks', JSON.stringify(todoItems));
+
+  // Re-fetch todoItems
+  fetchTodos();
+
+  // let deleteBtns = document.querySelectorAll('.delete-btn');
+  // for (let i = 0; i < deleteBtns.length; i++) {
+  //   deleteBtns[i].addEventListener('click', ($event) => {
+  //
+  //     let currentId = $event.target.parentElement.parentElement.id;
+  //     let todoItems = JSON.parse(localStorage.getItem('todos'));
+  //
+  //     for (let i = 0; i < todoItems.length; i++) {
+  //       if (todoItems[i].id === currentId) {
+  //         todoItems.splice(i, 1);
+  //       }
+  //     }
+  //     localStorage.setItem('todos', JSON.stringify(todoItems));
+  //     refreshDOMTable();
+  //   });
+  //
+  // }
+
+}
+
+
+// Fetch bookmarks
+function fetchTodos(){
+
+  let todoItems = JSON.parse(localStorage.getItem('todos'));
+  let tableContainer = document.getElementById('todos-table-container');
+  let tableBody = document.getElementById('tableBody');
+
+  if (todoItems) {
+    todoItems.forEach((el) => {
+
+      let currentRow = document.createElement('div');
+      let currentTitleTodo = document.createElement('div');
+      let currentDescription = document.createElement('div');
+      let currentPriority = document.createElement('div');
+      let currentStatusBtn = document.createElement('div');
+      let currentEditBtn = document.createElement('div');
+      let currentDeleteBtn = document.createElement('div');
+
+      currentRow.id = el.id;
+
+      currentStatusBtn.className = 'status-btn';
+      currentRow.className = 'todoItem';
+      currentEditBtn.className = 'edit-btn';
+      currentDeleteBtn.className = 'delete-btn';
+      currentTitleTodo.className = 'title-todo';
+      currentDescription.className = 'description-todo';
+      currentPriority.className = 'priority';
+
+      currentTitleTodo.innerHTML = el.title;
+      currentDescription.innerHTML = el.description;
+      currentDeleteBtn.innerHTML = '<i class="fas fa-trash"></i>';
+      currentEditBtn.innerHTML = '<i class="fas fa-edit"></i>';
+      currentPriority.innerHTML = el.priority;
+
+      currentRow.appendChild(currentTitleTodo);
+      currentRow.appendChild(currentDescription);
+      currentRow.appendChild(currentPriority);
+      currentRow.appendChild(currentStatusBtn);
+      currentRow.appendChild(currentEditBtn);
+      currentRow.appendChild(currentDeleteBtn);
+
+      tableBody.appendChild(currentRow);
+    });
+
+
+
+
+    // // Get todoItems from localStorage
+    // let todoItems = JSON.parse(localStorage.getItem('todos'));
+    // // Loop through the todoItems
+    // for(let i =0; i < todoItems.length; i++){
+    //   if(todoItems[i].id === id){
+    //     // Remove from array
+    //     todoItems.splice(i, 1);
+    //   }
+    // }
+    // // Re-set back to localStorage
+    // localStorage.setItem('bookmarks', JSON.stringify(todoItems));
+  }
+  let deleteBtns = document.querySelectorAll('.delete-btn');
+
+  for (let i = 0; i < deleteBtns.length; i++) {
+    deleteBtns[i].addEventListener('click', ($event) => {
+
+      let currentId = $event.target.parentElement.parentElement.id;
+      let todoItems = JSON.parse(localStorage.getItem('todos'));
+
+      for (let i = 0; i < todoItems.length; i++) {
+        if (todoItems[i].id === currentId) {
+          todoItems.splice(i, 1);
+        }
+      }
+      localStorage.setItem('todos', JSON.stringify(todoItems));
+      fetchTodos();
+    });
+
+  }
+
+
+  // tableBody.innerHTML = '';
+  // for (let i = 0; i < todoItems.length; i++) {
+  //   let title = todoItems[i].title;
+  //   let description = todoItems[i].description;
+  //   let option = todoItems[i].option;
+  //   let id = todoItems[i].id;
+  //
+  //   tableBody.innerHTML += '<div class="well">'+
+  //   '<h3>'+title+
+  //   '<div>\''+description+'\</div>'+
+  //   '<button onclick="deleteCurrentTodoItem(\''+id+'\')" class="btn btn-danger">Delete</button>' +
+  //   '</h3>'+
+  //
+  //   '</div>';
+  // }
+
+  // // Get bookmarks from localStorage
+  // var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+  // // Get output id
+  // var bookmarksResults = document.getElementById('bookmarksResults');
+  //
+  // // Build output
+  // bookmarksResults.innerHTML = '';
+  // for(var i = 0; i < bookmarks.length; i++){
+  //   var name = bookmarks[i].name;
+  //   var url = bookmarks[i].url;
+  //
+  //   bookmarksResults.innerHTML += '<div class="well">'+
+  //       '<h3>'+name+
+  //       ' <a class="btn btn-default" target="_blank" href="'+addhttp(url)+'">Visit</a> ' +
+  //       ' <a onclick="deleteBookmark(\''+url+'\')" class="btn btn-danger" href="#">Delete</a> ' +
+  //       '</h3>'+
+  //       '</div>';
+  // }
+  // fetchTodos();
+}
+fetchTodos();
+
+
 
 
 
